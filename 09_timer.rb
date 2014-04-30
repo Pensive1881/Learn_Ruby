5912ccd1fda81fdefd7f812ce61233c94b48cf29
Pensive1881 <lumberjackparade@hotmail.com>
2014-04-29 20:27:08 -0700

    Update 09_timer.rb

--- 09_timer.rb
+++ 09_timer.rb
@@ -9,9 +9,9 @@ class Timer
 
   def time_string
     min = @seconds / 60
-	@seconds %= 60
-	hours = min / 60
-	min %= 60
+    @seconds %= 60
+    hours = min / 60
+    min %= 60
     time_string = "%02d:%02d:%02d" % [hours, min, @seconds]
   end
 end
