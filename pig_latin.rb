901c94d563ebb3e2970639599ddcf62ef0f08355
Pensive1881 <lumberjackparade@hotmail.com>
2014-04-29 19:58:29 -0700

    Create pig_latin.rb

---
+++ pig_latin.rb
@@ -0,0 +1,19 @@
+def translate(string)
+  words = string.split(' ')
+  i = 0
+  while i < words.size
+    letters = words[i].split('')
+	cons = ""
+    letters.each do |i|
+	  if cons[-1] == 'q' && i == 'u'
+	   cons << i
+	  end
+	  break if i =~ /[aeiou]/
+      cons << i
+    end
+    words[i] = letters.join << cons << 'ay'
+	words[i] = words[i].reverse.chomp(cons.reverse).reverse
+	i += 1
+  end
+  words.join(' ')
+end
