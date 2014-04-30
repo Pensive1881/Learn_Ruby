84b0b98863fcdba014171fcc6b7b44d61427aab2
Pensive1881 <lumberjackparade@hotmail.com>
2014-04-29 20:26:27 -0700

    Update 08_book.rb

--- 08_book.rb
+++ 08_book.rb
@@ -1,16 +1,16 @@
 class Book
   def title=(stuff)
     stuff = stuff.split(' ')
-	stuff.map! do |x|
-	  if ['and','a','an','the','of','in'].include?(x)
-	    x
-	  elsif x=='i'
-	    x.capitalize
-	  else
-	    x.capitalize
-	  end
-	end
-	stuff[0].capitalize!
+    stuff.map! do |x|
+      if ['and','a','an','the','of','in'].include?(x)
+        x
+      elsif x=='i'
+        x.capitalize
+      else
+        x.capitalize
+      end
+    end
+    stuff[0].capitalize!
     @book = stuff.join(' ')
   end
   
