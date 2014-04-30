cbac46c2bb2708b8d432ba4209d5f4b7b6575513
Pensive1881 <lumberjackparade@hotmail.com>
2014-04-29 20:02:31 -0700

    Create book.rb

---
+++ book.rb
@@ -0,0 +1,20 @@
+class Book
+  def title=(stuff)
+    stuff = stuff.split(' ')
+	stuff.map! do |x|
+	  if ['and','a','an','the','of','in'].include?(x)
+	    x
+	  elsif x=='i'
+	    x.capitalize
+	  else
+	    x.capitalize
+	  end
+	end
+	stuff[0].capitalize!
+    @book = stuff.join(' ')
+  end
+  
+  def title
+    @book
+  end
+end
