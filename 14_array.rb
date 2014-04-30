d22c14724161ed211964df74aed42bd0786af1c1
Pensive1881 <lumberjackparade@hotmail.com>
2014-04-29 20:44:39 -0700

    Update 14_array.rb

--- 14_array.rb
+++ 14_array.rb
@@ -1,12 +1,12 @@
 class Array
   def sum
     if self == []
-	    0
-  	else
-	    num = 0
-	    self.each {|x| num+=x}
-	    num
-  	end
+      0
+    else
+      num = 0
+      self.each {|x| num+=x}
+      num
+    end
   end
 
   def square
